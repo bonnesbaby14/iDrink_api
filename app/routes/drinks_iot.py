331f35cc67b08ffcf44b3928e006b0ec1f6ae785
Gabriel Angeles <bonnesbaby14@gmail.com>
2023-04-22 17:21:56 -0600

    ajuste

--- app/routes/drinks_iot.py
+++ app/routes/drinks_iot.py
@@ -4,8 +4,10 @@ router = APIRouter()
 
 @router.get("/serve/{drink_id}")
 async def read_item(drink_id: int, q: str = None):
+    print("etre")
     return {"drink_id": drink_id, "q": q}
 
 @router.get("/")
 async def read_item():
+    print("etre")
     return {"drink_id":"hola"}
