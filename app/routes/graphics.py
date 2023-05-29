4cc9220a92d2148e31fff1613dd668a3a1908137
bonnesbaby14 <bonnesbaby14@gmail.com>
2023-05-28 19:50:38 -0600

    ajuste

--- app/routes/graphics.py
+++ app/routes/graphics.py
@@ -22,25 +22,26 @@ router = APIRouter()
 @router.get("/graphics_status")
 async def read_item():
     status = session.query(Status).order_by(Status.id.desc()).first()
-    
+
     if status:
         values = [float(status.bottle1), float(status.bottle2), float(status.bottle3), float(status.bottle4)]
-        labels = ['Value 1', 'Value 2', 'Value 3', 'Value 4']
-        
+        labels = ['Bottle 1', 'Bottle 2', 'Bottle 3', 'Bottle 4']
+
         df = pd.DataFrame({'Values': values}, index=labels)
-        
-        # Generar gráfica de barras
-        ax = df.plot(kind='bar')
-        ax.set_xlabel('Values')
-        ax.set_ylabel('Count')
-        ax.set_title('Bar Chart')
-        
+
+        # Generar gráfica de barras con colores personalizados
+        colors = ['blue', 'green', 'red', 'yellow']
+        ax = df.plot(kind='bar', color=colors)
+        ax.set_xlabel('Botellas')
+        ax.set_ylabel('mililitros')
+        ax.set_title('Niveles de botellas')
+
         # Convertir la gráfica a una imagen en formato Base64
         buffer = io.BytesIO()
         plt.savefig(buffer, format='png')
         buffer.seek(0)
         image_base64 = base64.b64encode(buffer.getvalue()).decode()
-        
+
         return JSONResponse(content={"data": jsonable_encoder(status), "image_base64": "data:image/png;base64," + image_base64}, status_code=200)
     else:
         return JSONResponse(content={"message": "No records found"}, status_code=404)
